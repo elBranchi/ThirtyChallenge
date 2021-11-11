61c9b77a812c426b701aa9ad410f45fa3518c70a
David Branchat Uriarte <spounge@gmail.com>
2021-11-11 01:19:12 +0100

    Third challenge solution proposal

---
+++ Readme.md
@@ -0,0 +1,13 @@
+
+This solutions contains 3 projects:
+* ThirtyChallenge
+* FollowChallenge
+* ThirdOption
+
+I started developing the challenge with VS2022 preview & was lucky that 1st non preview VS2022 was release same week ^_^'
+The idea for using VS2022 was taking advantage of C# 10 /.Net Core 6 & [Top level statements](https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/tutorials/top-level-statements)
+
+
+These project are possible answers to the challenge presented 
+here ( **I tried to follow the rules, but probably I have breached some in some way, I was just really trying to have fun :D**) :
+![Challenge](images/Hello_Challenge.jpg)
---
+++ ThirdOption/Program.cs
@@ -0,0 +1,25 @@
+﻿var letterPak = new Dictionary<char, (int w, long bm)> { { '!', (1, 0x3d) }, { ',', (2, 0x6) }, { 'l', (3, 0x32498 /*0x6498*/) }, { ' ', (4, 0x0) }, { 'e', (5, 0xe8d1c0) }, { 'o', (5, 0xe8c5c0) }, { 'r', (5, 0x16cc200) }, { 'd', (5, 0x62749c0) }, { 'H', (6, 0x861fe1840) }, { 'W', (6, 0x861b6d480) }, };
+var msg = "Hello, World!";
+void WriteItWPack(string msg, Dictionary<char, (int w, long bm)> letterPak)
+{
+    var c = Console.WindowLeft;
+    var r = Console.WindowTop;
+    var bits = msg.Select(c => letterPak[c]);
+    foreach(var cm in bits)
+    {
+        if(c +cm.w > Console.WindowWidth)
+        {
+            c = Console.WindowLeft;
+            r += 6;
+        }
+        foreach(var b in Enumerable.Range(0, cm.w * 6))
+        {
+            var m = (1L << (cm.w * 6) - 1) >> b;
+            Console.SetCursorPosition(c + b % cm.w, r + b / cm.w);
+            Console.Write((cm.bm & m) != 0 ? '#': ' ');
+        }
+        c += cm.w + 1;
+    }
+}
+Console.Clear();
+WriteItWPack(msg, letterPak);
